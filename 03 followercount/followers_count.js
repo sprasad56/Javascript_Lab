9a4894b2c95976752eca367fdc89172ee38fe353
Sivaprasad.G <sivaprasad.g@live.com>
2024-05-05 20:25:32 +0530

    updated

---
+++ 03 followercount/followers_count.js
@@ -0,0 +1,16 @@
+let count = 0;
+function increaseCount() {
+  count++;
+  displayCount();
+  checkCountValue();
+}
+function displayCount() {
+  document.getElementById("countDisplay").innerHTML = count; // Display the count in the HTML
+}
+function checkCountValue() {
+  if (count === 10) {
+    alert("Your Instagram post gained 10 followers! Congratulations!");
+  } else if (count === 20) {
+    alert("Your Instagram post gained 20 followers! Keep it up!");
+  }
+}
